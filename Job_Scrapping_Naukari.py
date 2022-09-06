ffdba9b9c5b479275d1e242a5cc68cf8ec24b602
Tejas Tendulkar <tejast.hlc@homelane.com>
2022-09-06 09:21:10 +0530

    tejas:fixes

--- Job_Scrapping_Naukari.py
+++ Job_Scrapping_Naukari.py
@@ -4,14 +4,60 @@ import time
 import random
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import csv
+import logging
 
 
 
-class ScrapNaukriJobs:
+class CSVHeaders:
+    CSV_DESIGNATION='Designation'
+    CSV_COMPANY_NAME = 'Company Name'
+    CSV_SALARY = 'Salary'
+    CSV_EXPERIENCE = 'Experience'
+    CSV_LOCATION = 'Location'
+    CSV_ROLE = 'Role'
+    CSV_SKILL = 'Skills'
+    CSV_QUALIFICATION = 'Qualification'
+    CSV_INDUSTRY_TYPE = 'Industry Type'
+    CSV_FUNCTIONAL_AREA = 'Functional Area'
+    CSV_EMPLOYMENT_TYPE = 'Employment Type'
+    CSV_ROLE_CATEGORIE = 'Role Category'
+    CSV_ADDRESS = 'Address'
+    CSV_POST_BY = 'Post By'
+    CSV_POST_DATE = 'Post Date'
+    CSV_WEBSITE = 'Website'
+    CSV_URL = 'Url'
+    CSV_ABOUT_COMPANY = 'About Company'
+    CSV_JOB_DESCRIPTION = 'Job Description'
+
+
+class ScrapNaukriJobs(CSVHeaders):   
+
     BASE_URL = 'https://www.naukri.com/'
     FILE_NAME = 'scrap_naukri_jobs.csv'
     CTC_FILTER_QUERY_PARAMS = '&ctcFilter=101&ctcFilter=15to25&ctcFilter=25to50&ctcFilter=50to75&ctcFilter=75to100'
     CITY_FILTER_PARAMS = '&cityTypeGid=6&cityTypeGid=17&cityTypeGid=73&cityTypeGid=97&cityTypeGid=134&cityTypeGid=139&cityTypeGid=183&cityTypeGid=220&cityTypeGid=232&cityTypeGid=9508&cityTypeGid=9509'
+    HEADERS_LIST = [
+            CSVHeaders.CSV_DESIGNATION,
+            CSVHeaders.CSV_COMPANY_NAME,
+            CSVHeaders.CSV_SALARY,
+            CSVHeaders.CSV_EXPERIENCE,
+            CSVHeaders.CSV_LOCATION,
+            CSVHeaders.CSV_ROLE,
+            CSVHeaders.CSV_SKILL,
+            CSVHeaders.CSV_QUALIFICATION,
+            CSVHeaders.CSV_INDUSTRY_TYPE,
+            CSVHeaders.CSV_FUNCTIONAL_AREA,
+            CSVHeaders.CSV_EMPLOYMENT_TYPE,
+            CSVHeaders.CSV_ROLE_CATEGORIE,
+            CSVHeaders.CSV_ADDRESS,
+            CSVHeaders.CSV_POST_BY,
+            CSVHeaders.CSV_POST_DATE,
+            CSVHeaders.CSV_WEBSITE,
+            CSVHeaders.CSV_URL,
+            CSVHeaders.CSV_ABOUT_COMPANY,
+            CSVHeaders.CSV_JOB_DESCRIPTION,
+        ]
 
     def __init__(self,language):
         options = webdriver.ChromeOptions()
@@ -23,7 +69,7 @@ class ScrapNaukriJobs:
         self.job_detail_links = []
 
     def get_job_detail_links(self):
-        for page in range(1,4):
+        for page in range(1,2):
             query_param = f'{self.language}-jobs'
             URL = f"{self.BASE_URL}{query_param}?k={self.language}{self.CTC_FILTER_QUERY_PARAMS}{self.CITY_FILTER_PARAMS}" if page == 1 else f"{self.BASE_URL}{query_param}-{str(page)}?k={self.language}{self.CTC_FILTER_QUERY_PARAMS}{self.CITY_FILTER_PARAMS}"            
             self.driver.get(URL)            
@@ -43,91 +89,73 @@ class ScrapNaukriJobs:
         key_skill_list,about_company_list,address_list,post_by_list = [],[],[],[]       
         post_date_list,website_list,url_list = [],[],[]
 
-
-        for link in range(len(self.job_detail_links)):
-            print(link,'...')
-            time.sleep(10)
-            self.driver.get(self.job_detail_links[link])    
-            soup=BeautifulSoup(self.driver.page_source, 'lxml')
-
-            if soup.find(attrs={'class':"salary"})==None: 
-                continue
-            else:   
-
-                import pdb;pdb.set_trace()
-
-                company_name_list.append("NA" if soup.find(attrs={'class':"jd-header-comp-name"}) == None else soup.find(attrs={'class':"jd-header-comp-name"}).find(attrs={'class':"pad-rt-8"}).text)                
-                experience_list.append("NA" if soup.find(attrs={'class':"exp"}) == None else soup.find(attrs={'class':"exp"}).text)
-                salary_list.append("NA" if soup.find(attrs={'class':"salary"})== None else soup.find(attrs={'class':"salary"}).text)
-                location_list.append("NA" if soup.find(attrs={'class':'loc'}) == None else soup.find(attrs={'class':'loc'}).find('a').text)
-                designation_list.append("NA" if soup.find(attrs={'class':"jd-header-title"}) == None else soup.find(attrs={'class':"jd-header-title"}).text)
-                job_description_list.append("NA" if soup.find(attrs={'class':"job-desc"})==None else soup.find(attrs={'class':"job-desc"}).text)
-                post_date_list.append(["NA"] if soup.find(attrs={'class':"jd-stats"}) == None else [i for i in soup.find(attrs={'class':"jd-stats"})][0].text.split(':')[1])
-                website_list.append("NA" if soup.find(attrs={'class':"jd-header-comp-name"}) == None else soup.find(attrs={'class':"jd-header-comp-name"}).contents[0]['href'])
-                url_list.append("NA" if soup.find(attrs={'class':"jd-header-comp-name"}) == None else soup.find(attrs={'class':"jd-header-comp-name"}).contents[0]['href'])      
-                         
-
-                details=[]
-                for i in soup.find(attrs={'class':"other-details"}).findAll(attrs={'class':"details"}):
-                    details.append(i.text)
-
-                role_list.append(details[0])
-                industry_type_list.append(details[1])
-                functional_area_list.append(details[2])
-                employment_type_list.append(details[3])
-                role_category_list.append(details[4])
-
-                qual=[]
-                for i in soup.find(attrs={'class':"education"}).findAll(attrs={'class':'details'}):
-                    qual.append(i.text)
-                education_list.append(qual)
-
-                sk=[]
-                for i in soup.find(attrs={'class':"key-skill"}).findAll('a'):
-                    sk.append(i.text)
-                key_skill_list.append(",".join(sk))                
-
-                if soup.find(attrs={'class':"name-designation"})==None:
-                    post_by_list.append("NA")
-                else:
-                    post_by_list.append(soup.find(attrs={'class':"name-designation"}).text)
-
-                if soup.find(attrs={'class':"about-company"})==None:                    
-                    about_company_list.append("NA")                    
-                else:                    
-                    address_list.append("NA" if soup.find(attrs={'class':"about-company"}).find(attrs={'class':"comp-info-detail"}) == None else soup.find(attrs={'class':"about-company"}).find(attrs={'class':"comp-info-detail"}).find('span').text)
-                    about_company_list.append(soup.find(attrs={'class':"about-company"}).find(attrs={'class':"detail dang-inner-html"}).text)
-
-                
-
-
-
-        df=pd.DataFrame()
-        df['Designation'] = designation_list
-        df['Company Name'] = company_name_list
-        df['Salary']=salary_list
-        df['Experience']=experience_list
-        df['Location']=location_list
-        df['Role']=role_list
-        df['Skills']=key_skill_list
-        df['Qualification']=education_list
-        df['Industry Type']=industry_type_list
-        df['Functional Area']=functional_area_list
-        df['Employment Type']=employment_type_list
-        df['Role Category']=role_category_list
-        df['Address'] = address_list
-        df['Post By'] = post_by_list
-        df['Post Date'] = post_date_list
-        df['Website'] = website_list
-        df['Url'] = url_list
-        df['Job Description']=job_description_list
-        df['About Company']=about_company_list
-        df.to_csv(self.FILE_NAME,index=False)
-        self.driver.close()
+        
         
 
-print("Program star time...",time.time())
+        with open(self.FILE_NAME, 'a') as csv_file:
+            dict_object = csv.DictWriter(csv_file, fieldnames=self.HEADERS_LIST)
+            dict_object.writeheader()
+            for link in range(len(self.job_detail_links)):
+                context = {}
+                time.sleep(10)
+                self.driver.get(self.job_detail_links[link]) 
+                soup=BeautifulSoup(self.driver.page_source, 'lxml')              
+                
+
+                if soup.find(attrs={'class':"salary"})==None: 
+                    continue
+                else:
+                    context.update({self.CSV_DESIGNATION:"NA" if soup.find(attrs={'class':"jd-header-title"}) == None else soup.find(attrs={'class':"jd-header-title"}).text})
+                    context.update({self.CSV_COMPANY_NAME:"NA" if soup.find(attrs={'class':"jd-header-comp-name"}) == None else soup.find(attrs={'class':"jd-header-comp-name"}).find(attrs={'class':"pad-rt-8"}).text})                    
+                    context.update({self.CSV_SALARY:"NA" if soup.find(attrs={'class':"salary"})== None else soup.find(attrs={'class':"salary"}).text})
+                    context.update({self.CSV_EXPERIENCE:"NA" if soup.find(attrs={'class':"exp"}) == None else soup.find(attrs={'class':"exp"}).text})
+                    context.update({self.CSV_LOCATION:"NA" if soup.find(attrs={'class':'loc'}) == None else soup.find(attrs={'class':'loc'}).find('a').text})
+
+                    details=[]
+                    for i in soup.find(attrs={'class':"other-details"}).findAll(attrs={'class':"details"}):
+                        details.append(i.text)
+                    context.update({self.CSV_ROLE:details[0],
+                        self.CSV_INDUSTRY_TYPE:details[1],
+                        self.CSV_FUNCTIONAL_AREA:details[2],
+                        self.CSV_EMPLOYMENT_TYPE:details[3],
+                        self.CSV_ROLE_CATEGORIE:details[4]                
+                    })
+                    
+                    context.update({self.CSV_JOB_DESCRIPTION:"NA" if soup.find(attrs={'class':"job-desc"})==None else soup.find(attrs={'class':"job-desc"}).text})
+                    context.update({self.CSV_POST_DATE :["NA"] if soup.find(attrs={'class':"jd-stats"}) == None else [i for i in soup.find(attrs={'class':"jd-stats"})][0].text.split(':')[1]})
+                    context.update({self.CSV_WEBSITE:"NA" if soup.find(attrs={'class':"jd-header-comp-name"}) == None else soup.find(attrs={'class':"jd-header-comp-name"}).contents[0]['href']})
+                    context.update({self.CSV_URL:"NA" if soup.find(attrs={'class':"jd-header-comp-name"}) == None else soup.find(attrs={'class':"jd-header-comp-name"}).contents[0]['href']})
+                           
+
+                    qual=[]
+                    for i in soup.find(attrs={'class':"education"}).findAll(attrs={'class':'details'}):
+                        qual.append(i.text)
+                    context.update({self.CSV_QUALIFICATION:qual}) 
+                    
+
+                    sk=[]
+                    for i in soup.find(attrs={'class':"key-skill"}).findAll('a'):
+                        sk.append(i.text)                     
+                    context.update({self.CSV_SKILL:",".join(sk)})      
+
+                    if soup.find(attrs={'class':"name-designation"})==None:
+                        context.update({self.CSV_POST_BY:"NA"})         
+                    else:
+                        context.update({self.CSV_POST_BY:soup.find(attrs={'class':"name-designation"}).text})
+
+
+
+                    if soup.find(attrs={'class':"about-company"})==None:                    
+                        context.update({self.CSV_ABOUT_COMPANY:"NA"})
+                    else:
+                        context.update({self.CSV_ADDRESS:"NA" if soup.find(attrs={'class':"about-company"}).find(attrs={'class':"comp-info-detail"}) == None else soup.find(attrs={'class':"about-company"}).find(attrs={'class':"comp-info-detail"}).find('span').text})                      
+                        context.update({self.CSV_ABOUT_COMPANY:soup.find(attrs={'class':"about-company"}).find(attrs={'class':"detail dang-inner-html"}).text})
+                dict_object.writerow(context)
+
+        
+logging.warning("{0} Program start time...".format(time.time()))
 scrap_naukri = ScrapNaukriJobs("PYTHON")
 scrap_naukri.scrap_details()
-# scrap_naukri.get_job_detail_links()
-print("Execution completed...",time.time())
+logging.warning("{0} Execution completed...".format(time.time()))
+
+
