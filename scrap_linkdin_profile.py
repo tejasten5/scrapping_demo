53445d50e16bab0f2e5fdac6e953489e0179c1de
Tejas Tendulkar <tejast.hlc@homelane.com>
2022-09-19 01:00:18 +0530

    tejas:fixes

--- scrap_linkdin_profile.py
+++ scrap_linkdin_profile.py
@@ -61,7 +61,9 @@ class ScrapLinkdinJobs:
         self.driver.close()
         
     def fetch_data(self,soup,dict_object,context):
+        print("........")
         for html in soup.find(attrs={'class':'reusable-search__entity-result-list list-style-none'}).findAll(attrs={'class':'reusable-search__result-container'}):
+            print("-----")
             try:
                 profile_url = html.find(attrs={'class':'app-aware-link scale-down'})['href']
             except Exception as e:
@@ -117,78 +119,123 @@ class ScrapLinkdinJobs:
             for company in companies_code:                   
                 for designation in designations:
                     context = {}
-                    page = 100
-                    has_pagination = False
 
-                    for page_num in range(1,page):
-
-                        if page_num == 1:
-                            url = '''https://www.linkedin.com/search/results/people/?currentCompany=%5B%22{linkdin_company_code}%22%5D&keywords={company_name}&origin=FACETED_SEARCH&title={designation}
-                            '''.format(linkdin_company_code = company[1],company_name=company[0],designation=designation.replace(' ','%20') if designation.isspace() else designation)
-                        else:
-                            url = '''https://www.linkedin.com/search/results/people/?currentCompany=%5B%22{linkdin_company_code}%22%5D&keywords={company_name}&origin=FACETED_SEARCH&title={designation}&&page={page_numbers}
-                            '''.format(linkdin_company_code = company[1],company_name=company[0],designation=designation.replace(' ','%20') if designation.isspace() else designation,page_numbers=page_num)
+                    url = '''https://www.linkedin.com/search/results/people/?currentCompany=%5B%22{linkdin_company_code}%22%5D&keywords={company_name}&origin=FACETED_SEARCH&title={designation}
+                    '''.format(linkdin_company_code = company[1],company_name=company[0],designation=designation.replace(' ','%20') if designation.isspace() else designation)
                         
-                        print(url,"<<<--------------------------------------------------------------------------------------->>>")                        
-                        self.driver.get(url)
+                    self.driver.get(url)
+                    time.sleep(120)
 
-                        soup=BeautifulSoup(self.driver.page_source, 'html.parser')
+                    soup=BeautifulSoup(self.driver.page_source, 'html.parser')
                     
 
-                        if soup.find(attrs={'class':'reusable-search-filters__no-results artdeco-card mb2'}):
-                            continue
+                    if soup.find(attrs={'class':'reusable-search-filters__no-results artdeco-card mb2'}):
+                        continue
                         
-                        if not has_pagination:
-                            total_records = int(soup.find(attrs={'class':'pb2 t-black--light t-14'}).get_text().strip('\n').split()[0])    
-                            page = round(total_records/self.TOTAL_RECORDS_ON_SINGLE_PAGE)
+                    # total_records = int(soup.find(attrs={'class':'pb2 t-black--light t-14'}).get_text().strip('\n').split()[0])    
+                    # page = round(total_records/self.TOTAL_RECORDS_ON_SINGLE_PAGE)                      
+                    # has_pagination = False
+                    
+
                         
+                    # if page > 0 and has_pagination:
+                    #     print("....................................IF condition executing.............................")
+                    #     print("next pagination..")
+                    #     for page_num in range(1,page+1):
+                    #         print(page_num,"|||||||||||||||||||||||||||||||||||||||||||||||")
+                    #         url = '''https://www.linkedin.com/search/results/people/?currentCompany=%5B%22{linkdin_company_code}%22%5D&keywords={company_name}&origin=FACETED_SEARCH&title={designation}&page={page_num_size}
+                    #             '''.format(linkdin_company_code = company[1],company_name=company[0],designation=designation.replace(' ','%20') if designation.isspace() else designation,page_num_size=page_num)
+                    #         print(url,"........................URL...................")
+                    #         self.driver.get(url)
 
-                        if page > 0:
-                            has_pagination = True
+                    #         if soup.find(attrs={'class':'reusable-search-filters__no-results artdeco-card mb2'}):
+                    #             continue
 
-                        # self.fetch_data(soup,dict_object,context)
-                    print(page,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")                   
+                    #         ###################################################################################################################
+                    #         for html in soup.find(attrs={'class':'reusable-search__entity-result-list list-style-none'}).findAll(attrs={'class':'reusable-search__result-container'}):                                              
+                    #             try:
+                    #                 profile_url = html.find(attrs={'class':'app-aware-link scale-down'})['href']
+                    #             except Exception as e:
+                    #                 profile_url = "NA"
 
-                    # for html in soup.find(attrs={'class':'reusable-search__entity-result-list list-style-none'}).findAll(attrs={'class':'reusable-search__result-container'}):                      
+                    #             try:
+                    #                 name = html.findAll('span',attrs={'aria-hidden':'true'})[0].get_text()                        
+                    #                 name_list = name.split()                                
+                    #                 # fname,lname = name_list.pop(0)," ".join(name_list) if len(name_list) > 2 else name_list[0],name_list[1]
+                    #                 if len(name_list) > 2:
+                    #                     fname,lname = name_list.pop(0)," ".join(name_list)                            
+                    #                 else:
+                    #                     fname,lname = name_list[0],name_list[1]
+                    #             except Exception as e:
+                    #                 fname = "NA"
+                    #                 lname = "NA"
+                                    
+                    #             try:
+                    #                 city = html.find(attrs = {'class':'entity-result__secondary-subtitle t-14 t-normal'}).get_text().strip('\n')
+                    #             except Exception as e:
+                    #                 city = "NA"
 
-                    #     try:
-                    #         profile_url = html.find(attrs={'class':'app-aware-link scale-down'})['href']
-                    #     except Exception as e:
-                    #         profile_url = "NA"
+                    #             try:
+                    #                 designation = html.find(attrs={'class':'entity-result__primary-subtitle t-14 t-black t-normal'}).get_text().strip('\n')
+                    #             except Exception as e:
+                    #                 designation = "NA"
 
-                    #     try:
-                    #         name = html.findAll('span',attrs={'aria-hidden':'true'})[0].get_text()                        
-                    #         name_list = name.split()                                
-                    #         # fname,lname = name_list.pop(0)," ".join(name_list) if len(name_list) > 2 else name_list[0],name_list[1]
-                    #         if len(name_list) > 2:
-                    #             fname,lname = name_list.pop(0)," ".join(name_list)                            
-                    #         else:
-                    #             fname,lname = name_list[0],name_list[1]
-                    #     except Exception as e:
-                    #         fname = "NA"
-                    #         lname = "NA"
+
+                    #             context.update({
+                    #                 LinkdinHeaders.LD_FIRST_NAME:fname,
+                    #                 LinkdinHeaders.LD_LAST_NAME:lname,
+                    #                 LinkdinHeaders.LD_COMPANY_NAME:company[0],
+                    #                 LinkdinHeaders.LD_DESIGNATIONS:designation,
+                    #                 LinkdinHeaders.LD_CITIES:city,
+                    #                 LinkdinHeaders.LD_URLS:profile_url,
+                    #                 LinkdinHeaders.LD_COUNTRIES:""
+                    #             })                            
+                    #             dict_object.writerow(context) 
+
+                            ###################################################################################################################
+
+                    print("........................ELSE CODE EXECUTING...........")
+                    for html in soup.find(attrs={'class':'reusable-search__entity-result-list list-style-none'}).findAll(attrs={'class':'reusable-search__result-container'}):                      
+                        
+                        try:
+                            profile_url = html.find(attrs={'class':'app-aware-link scale-down'})['href']
+                        except Exception as e:
+                            profile_url = "NA"
+
+                        try:
+                            name = html.findAll('span',attrs={'aria-hidden':'true'})[0].get_text()                        
+                            name_list = name.split()                                
+                            # fname,lname = name_list.pop(0)," ".join(name_list) if len(name_list) > 2 else name_list[0],name_list[1]
+                            if len(name_list) > 2:
+                                fname,lname = name_list.pop(0)," ".join(name_list)                            
+                            else:
+                                fname,lname = name_list[0],name_list[1]
+                        except Exception as e:
+                            fname = "NA"
+                            lname = "NA"
                             
-                    #     try:
-                    #         city = html.find(attrs = {'class':'entity-result__secondary-subtitle t-14 t-normal'}).get_text().strip('\n')
-                    #     except Exception as e:
-                    #         city = "NA"
+                        try:
+                            city = html.find(attrs = {'class':'entity-result__secondary-subtitle t-14 t-normal'}).get_text().strip('\n')
+                        except Exception as e:
+                            city = "NA"
 
-                    #     try:
-                    #         designation = html.find(attrs={'class':'entity-result__primary-subtitle t-14 t-black t-normal'}).get_text().strip('\n')
-                    #     except Exception as e:
-                    #         designation = "NA"
+                        try:
+                            designation = html.find(attrs={'class':'entity-result__primary-subtitle t-14 t-black t-normal'}).get_text().strip('\n')
+                        except Exception as e:
+                            designation = "NA"
 
 
-                    #     context.update({
-                    #         LinkdinHeaders.LD_FIRST_NAME:fname,
-                    #         LinkdinHeaders.LD_LAST_NAME:lname,
-                    #         LinkdinHeaders.LD_COMPANY_NAME:company[0],
-                    #         LinkdinHeaders.LD_DESIGNATIONS:designation,
-                    #         LinkdinHeaders.LD_CITIES:city,
-                    #         LinkdinHeaders.LD_URLS:profile_url,
-                    #         LinkdinHeaders.LD_COUNTRIES:""
-                    #     })                            
-                    #     dict_object.writerow(context)     
+                        context.update({
+                            LinkdinHeaders.LD_FIRST_NAME:fname,
+                            LinkdinHeaders.LD_LAST_NAME:lname,
+                            LinkdinHeaders.LD_COMPANY_NAME:company[0],
+                            LinkdinHeaders.LD_DESIGNATIONS:designation,
+                            LinkdinHeaders.LD_CITIES:city,
+                            LinkdinHeaders.LD_URLS:profile_url,
+                            LinkdinHeaders.LD_COUNTRIES:""
+                        })                            
+                        dict_object.writerow(context)
+                    # has_pagination = True     
                 time.sleep(30)
             time.sleep(30)
         time.sleep(30)
